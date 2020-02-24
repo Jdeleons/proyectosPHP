ae182bb217d86319bf18b7e9922e8990c126c6b7
Jdeleons <jdeleons3@miumg.edu.gt>
2020-02-23 19:20:56 -0600

    jdeleons
    comit desde usuario de dominio
    
    Co-Authored-By: jdeleons <jdeleons@users.noreply.github.com>

---
+++ confeccionesAdrys/vistas/inicio.php
@@ -0,0 +1,16 @@
+<?php 
+	include "header.php";
+ ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-4">
+			<h1>hola mundo</h1>
+		</div>
+	</div>
+</div>
+
+ <?php 
+ 	include "footer.php";
+
+  ?>
